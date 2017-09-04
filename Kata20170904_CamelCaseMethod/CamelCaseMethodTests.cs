9e99597626d123ba09f62cd378c4734967ce5ef8
cash <cash@cashwu.com>
2017-09-04 20:58:45 +0800

    init

--- Kata20170904_CamelCaseMethod/CamelCaseMethodTests.cs
+++ Kata20170904_CamelCaseMethod/CamelCaseMethodTests.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Kata20170904_CamelCaseMethod
 {
     [TestClass]
-    public class UnitTest1
+    public class CamelCaseMethodTests
     {
         [TestMethod]
         public void TestMethod1()
